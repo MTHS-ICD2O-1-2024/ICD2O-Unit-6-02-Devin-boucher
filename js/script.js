78bc9a47caa3dbb69504a025ef6abc376469e500
default <devin.boucher@stu.ocsb.ca>
2025-05-14 16:50:02 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Devin Boucher
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+// eslint-disable-next-line no-unused-vars
+
+// Cookie Clicker Logic
+function updateCookieCount() {
+  const count = localStorage.getItem("cookieCount") || 0
+  document.getElementById("cookie-count").textContent = count
+}
+
+function incrementCookieCount() {
+  let count = Number(localStorage.getItem("cookieCount")) || 0
+  count++
+  localStorage.setItem("cookieCount", count)
+  document.getElementById("cookie-count").textContent = count
+}
